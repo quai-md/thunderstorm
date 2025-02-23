33a0f37ab2dff54797a60dd005a20433476ab297
stav-adimor <stav@quai.md>
2025-02-23 16:11:38 +0200

    upadte component

--- thunderstorm/src/main/frontend/components/TS_CheckboxGroup/TS_CheckboxGroup.tsx
+++ thunderstorm/src/main/frontend/components/TS_CheckboxGroup/TS_CheckboxGroup.tsx
@@ -3,7 +3,7 @@ import {ComponentSync} from '../../core/ComponentSync';
 import {_className} from '../../utils/tools';
 import './TS_CheckboxGroup.scss';
 import {TS_Checkbox} from '../TS_Checkbox';
-import { LL_V_L } from '../Layouts';
+import {LL_V_L} from '../Layouts';
 import {BadImplementationException} from "@nu-art/ts-common";
 
 type CheckboxOption = {
@@ -15,14 +15,14 @@ type CheckboxOption = {
 export type Props_CheckboxGroup = {
     parent: CheckboxOption;
     options: CheckboxOption[];
-    selectedIds: string[];
-    onChange: (selectedIds: string[]) => void;
+    selectedIds: Map<string, string[]>;
+    onChange: (selectedIds: Map<string, string[]>) => void;
     id?: string;
     className?: string;
 };
 
 type State_CheckboxGroup = {
-    selectedIds: string[];
+    selectedIds: Map<string, string[]>;
     parent: CheckboxOption;
     options: CheckboxOption[];
     allSelected: boolean;
@@ -45,33 +45,43 @@ export class TS_CheckboxGroup extends ComponentSync<Props_CheckboxGroup, State_C
         if (!state.options.length)
             throw new BadImplementationException('cannot have checkbox group without options');
 
-        state.allSelected = state.selectedIds.length === state.options.length ?? false;
-        state.someSelected = !state.allSelected && state.selectedIds.length > 0;
-
         state.parent = nextProps.parent;
+
+        const parentId = state.parent.id;
+        const selectedIdsForParent = state.selectedIds.get(parentId) ?? [];
+
+        state.allSelected = selectedIdsForParent.length === state.options.length ?? false;
+        state.someSelected = !state.allSelected && selectedIdsForParent.length > 0;
+
         state.className = nextProps.className;
 
         return state;
     }
 
     private onClickFather = () => {
-        const { options, allSelected, selectedIds } = this.state;
+        const {options, allSelected, selectedIds, parent} = this.state;
+        const selectedIdsForParent = selectedIds.get(parent.id) ?? [];
 
         const selectableOptions = options.filter(option => !option.disabled);
-        const newSelectedIds = allSelected || selectableOptions.length === selectedIds.length ? new Set<string>() : new Set(selectableOptions.map(option => option.id));
+        const newSelectedIds = allSelected || selectableOptions.length === selectedIdsForParent.length ? [] : selectableOptions.map(option => option.id);
+        const parentToSelectedIdsMap = new Map<string, string[]>();
+        parentToSelectedIdsMap.set(parent.id, newSelectedIds);
 
-        this.props.onChange([...newSelectedIds]);
+        this.props.onChange(parentToSelectedIdsMap);
     };
 
-    private onClickCheckbox = (id: string) => {
-        const newSelectedIds = new Set(this.state.selectedIds);
-        newSelectedIds.has(id) ? newSelectedIds.delete(id) : newSelectedIds.add(id);
+    private onClickCheckbox = (childId: string, parentId: string) => {
+        const newSelectedIds = new Map(this.state.selectedIds);
+        const selectedIds = new Set(this.state.selectedIds.get(parentId));
 
-        this.props.onChange([...newSelectedIds]);
+        selectedIds.has(childId) ? selectedIds.delete(childId) : selectedIds.add(childId);
+        newSelectedIds.set(parentId, Array.from(selectedIds));
+
+        this.props.onChange(newSelectedIds);
     };
 
     render() {
-        const { selectedIds, someSelected, allSelected, options, parent, className } = this.state;
+        const {selectedIds, someSelected, allSelected, options, parent, className} = this.state;
 
         return (
             <LL_V_L className={_className('ts-checkbox-group', className)} id={this.props.id}>
@@ -86,9 +96,9 @@ export class TS_CheckboxGroup extends ComponentSync<Props_CheckboxGroup, State_C
                     {options.map(option => (
                         <TS_Checkbox
                             key={option.id}
-                            checked={selectedIds.includes(option.id)}
+                            checked={selectedIds.get(parent.id)?.includes(option.id)}
                             disabled={option.disabled || parent.disabled}
-                            onCheck={() => this.onClickCheckbox(option.id)}>
+                            onCheck={() => this.onClickCheckbox(option.id, parent.id)}>
                             {option.label}
                         </TS_Checkbox>
                     ))}
